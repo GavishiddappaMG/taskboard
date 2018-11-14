e9523b97edc5500bb1e42cbcece38568156886b1
gavishiddappa.gadogi <gavishiddappa.gadogi@impelsys.com>
2018-11-15 01:39:15 +0530

    removed unused component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,6 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  // {
-  //   path: 'home',
-  //   loadChildren: './core/core.module#CoreModule'
-  // },
   {
     path: 'taskboard',
     loadChildren: './taskboard/taskboard.module#TaskboardModule'
--- src/app/taskboard/taskboard.module.ts
+++ src/app/taskboard/taskboard.module.ts
@@ -4,11 +4,10 @@ import { DragDropModule } from '@angular/cdk/drag-drop';
 import { MatIconModule } from '@angular/material/icon';
 import { TaskboardRoutingModule } from './taskboard-routing.module';
 import { TaskboardComponent } from './taskboard/taskboard.component';
-import { TaskListComponent } from './task-list/task-list.component';
 import { WidgetModule } from '../widget/widget.module';
 
 @NgModule({
-  declarations: [TaskboardComponent, TaskListComponent],
+  declarations: [TaskboardComponent],
   imports: [
     CommonModule,
     TaskboardRoutingModule,
